122c0e88473e7d1987f92e0bf53df4e46c76c863
swapnar <swapnajr.3@gmail.com>
2019-06-19 11:12:38 +0530

    jsp commit

---
+++ src/main/java/com/thb/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.thb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			super.doPost(req, resp);
+		}
+
+}
